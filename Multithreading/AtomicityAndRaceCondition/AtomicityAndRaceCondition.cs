f56b65f9c794f02fb4c7761bf363e2e12156cb69
Maxim Korokin <maksimkorokin1@gmail.com>
2019-11-09 20:41:32 +0200

    Initial commit

---
+++ Multithreading/AtomicityAndRaceCondition/AtomicityAndRaceCondition.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Multithreading.AtomicityAndRaceCondition
+{
+    static class AtomicityAndRaceCondition
+    {
+        private static Bank bank = new Bank();
+        private static decimal defaultMoney = 1000;
+
+        public static void Run()
+        {
+            bank.CreateAccount("John", defaultMoney);
+            bank.CreateAccount("Alice", defaultMoney);
+            Console.WriteLine($"John has {defaultMoney} money and Alice has {defaultMoney} money.");
+            var readThread = new Thread(ReadFromBank);
+            var writeThread = new Thread(WriteToBank);
+            readThread.Start();
+            writeThread.Start();
+            readThread.Join();
+            writeThread.Join();
+        }
+
+        private static void ReadFromBank()
+        {
+            decimal johnMoney;
+            decimal aliceMoney;
+            for (int i = 0; i < 50000; i++)
+            {
+                johnMoney = bank.GetAccountMoney("John");
+                aliceMoney = bank.GetAccountMoney("Alice");
+                if (johnMoney + aliceMoney != defaultMoney * 2)
+                {
+                    Console.WriteLine($"Race condition: John has {johnMoney} money and Alice has {aliceMoney} money.");
+                }
+            }
+        }
+
+        private static void WriteToBank()
+        {
+            var rand = new Random();
+            for (int i = 0; i < 50000; i++)
+            {
+                bank.MakeTransaction("John", "Alice", rand.Next(1, 10));
+                bank.MakeTransaction("Alice", "John", rand.Next(1, 10));
+            }
+        }
+    }
+}
---
+++ Multithreading/AtomicityAndRaceCondition/Bank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithreading.AtomicityAndRaceCondition
+{
+    class Bank
+    {
+        private Dictionary<string, decimal> accounts = new Dictionary<string, decimal>();
+
+        // non-atomic operation
+        public void MakeTransaction(string fromAccount, string toAccount, decimal sum)
+        {
+            if (accounts[fromAccount] < sum)
+            {
+                return;
+            }
+            accounts[fromAccount] -= sum;
+            accounts[toAccount] += sum;
+        }
+
+        public decimal GetAccountMoney(string account)
+        {
+            return accounts[account];
+        }
+
+        public void CreateAccount(string account, decimal money)
+        {
+            accounts.Add(account, money);
+        }
+    }
+}
